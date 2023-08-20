c169ea438ad06a97aaa96ffa6be2457e67e4069b
nazimboudeffa <boudeffa@gmail.com>
2023-08-20 07:03:02 +0200

    Added code penal Algerien

--- mychatgpt.py
+++ mychatgpt.py
@@ -12,8 +12,11 @@ os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
 query = sys.argv[1]
 
-loader1 = TextLoader("data/data1.txt")
-loader2 = TextLoader("data/data2.txt")
-index = VectorstoreIndexCreator().from_loaders([loader1, loader2])
-
+loader1 = TextLoader("code/code-penal-1-dz.txt")
+loader2 = TextLoader("code/code-penal-2-dz.txt")
+loader3 = TextLoader("code/code-penal-3-dz.txt")
+loader4 = TextLoader("code/code-penal-4-dz.txt")
+#loader = TextLoader("data/quran.txt")
+index = VectorstoreIndexCreator().from_loaders([loader1, loader2, loader3, loader4])
+#index = VectorstoreIndexCreator().from_loaders([loader])
 print(index.query(query))
